ca9f178fafd1f8c2ca17936410c3a42dbdd7e1f8
Praharsh Jain <praharshsamria@gmail.com>
2017-01-31 23:35:24 +0530

    Added Quick Select

---
+++ Searching/quick_select.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+//To select kth smallest element in array
+//Complexity : O(n)
+T quick_select(T *arr, int k, int li, int hi)
+{
+    if (hi - li <= 1)
+        return arr[k];
+    int j = li;
+    swap(arr[j], arr[k]);
+    for (int i = j = li + 1; i < hi; i++)
+    {
+        if (arr[i] < arr[li])
+            swap(arr[j++], arr[i]);
+    }
+    swap(arr[--j], arr[li]);
+    if (k < j)
+        return quick_select(arr, k, li, j);
+    if (k > j)
+        return quick_select(arr, k - j, j + 1, hi);
+    return arr[j];
+}
+
+int main()
+{
+    int arr[] = {9, 1, 2, 4, 1};
+    cout << quick_select(arr, 3, 0, 5) << std::endl;
+    return 0;
+}
